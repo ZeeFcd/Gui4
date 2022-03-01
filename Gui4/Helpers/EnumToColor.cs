23f1e7df74aaae99b60fa0fa202469dbf7159eae
ZeeF <ZeeF@stud.uni-obuda.hu>
2022-03-01 18:25:19 +0100

    Enum to color convert done

---
+++ Gui4/Helpers/EnumToColor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Gui4.Helpers
+{
+    public class EnumToColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            KarmaEnum karma = (KarmaEnum)value;
+            switch (karma)
+            {
+                case KarmaEnum.Good:
+                    return Brushes.LightGreen;
+                    
+                case KarmaEnum.Bad:
+                    return Brushes.LightPink ;
+                case KarmaEnum.Neutral:
+                    return Brushes.LightYellow;
+                default:
+                    return Brushes.White;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
